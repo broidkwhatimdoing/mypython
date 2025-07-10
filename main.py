2a2708feb0c2626e7eade99da80ac3f9b8d18f10
broidkwhatimdoing <allisonzeng20100@gmail.com>
2025-07-10 15:37:11 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+_ = round(4*3.751)
+print(_)
+print(spam)
+
+print("This is Allison")
+
+text = "# is not a comment because it's inside quotes."
+print(text)
+
+spam=11
+scam=25
+total=spam+scam
+print(total)
+
+spam=50-5*2
+print(spam)
+
+print(17/3)
+print(17//3)
+print(17%3)
+print(2**7) 
+
+
+# 2025/7/10
+x=int (input("please enter an integer:"))
+if x<10 : print('single')
+if 100>x>9: print('double')
+if 1000>x>99:print('triple')
+
